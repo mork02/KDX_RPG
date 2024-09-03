46470f68e4b87509457936ac3a55c0dd895b6c84
mork <albers.mark02@gmail.com>
2024-09-03 02:13:42 +0200

    [CHANGE] window, text and optimization
    added window clear every tick, add user hp text into window (doesnt work correctly)

--- include/panel.h
+++ include/panel.h
@@ -1,14 +1,28 @@
 #pragma once
 #include "SFML/Graphics.hpp"
 #include "rect.h"
+#include "text.h"
+#include "player.h"
 
 class Panel
 {
 private:
-	Rect rect;
+	sf::RenderWindow& window;
+
+	Rect rect; 
+	Player user;
+
+	Text user_Health;
 
 public:
-	Panel();
-	void update(sf::RenderWindow& window);
+	Panel(sf::RenderWindow& window);
+
+	// Getter
+
+	// Setter
+
+	// Methods
+	void update();
+	void update_user_health();
 };
 
--- src/panel.cpp
+++ src/panel.cpp
@@ -1,11 +1,30 @@
 #include "panel.h"
 
-Panel::Panel() {
+Panel::Panel(sf::RenderWindow& window_c)
+	: window(window_c),
+	// Text
+	user_Health(std::to_string(user.get_stats().get_hp()) + " / " + std::to_string(user.get_stats().get_max_hp()), 42, 100, 100)
 
-}
-
-void Panel::update(sf::RenderWindow& window)
 {
-	rect.draw_rect(window);
+
 }
 
+void Panel::update()
+{
+	// clears the screen every frame
+	window.clear();
+
+	rect.draw_rect(window);
+	update_user_health();
+
+	user.get_stats().set_hp(-1);
+ 
+}
+
+void Panel::update_user_health()
+{
+	/*Takes the users HP and Max_HP and refreshes the string in Text. Draws the text again on window*/
+	user_Health.draw_text(window);
+	user.get_stats().set_hp(-1);
+	user_Health.set_text(std::to_string(user.get_stats().get_hp()) + " / " + std::to_string(user.get_stats().get_max_hp()));
+}
