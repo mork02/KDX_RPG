325cac681ad98a276df8b18a75b22449d09ff7f7
mork <albers.mark02@gmail.com>
2024-09-06 02:53:04 +0200

    [CHANGE] add menufields and hover on text and click on text method. add window and mouse

--- include/menu.h
+++ include/menu.h
@@ -2,28 +2,34 @@
 #include <iostream>
 #include "SFML/Graphics.hpp"
 #include "text.h"
+#include "controller.h"
 
 class Menu
 {
 private:
 	sf::RenderWindow& window;
+	Mouse_controller& mouse_controller;
 	sf::Texture texture;
 	sf::Sprite sprite;
-	Text menu_text;
+	Text menu_title_text;
+	Text menu_continue_text;
+	Text menu_option_text;
+	Text menu_quit_text;
 
 	std::string path = "assets/menu_assets/menu.png";
 
 	float xPos = 0;
 	float yPos = 0;
 	int scale = 6;
-
 	bool visable = false;
 
 public:
-	Menu(sf::RenderWindow& window);
-	bool get_visable();
+	Menu(sf::RenderWindow& window, Mouse_controller& mouse_ctrl);
+	bool get_visable() const;
+	sf::FloatRect get_text_bounds(Text text);
 
 	void set_visable(bool value);
 	void draw_menu();
 	void center_menu(); 
+	void update_text_hover();
 };
--- src/menu.cpp
+++ src/menu.cpp
@@ -1,6 +1,11 @@
 #include "menu.h"
 
-Menu::Menu(sf::RenderWindow& window_c) : window(window_c), menu_text("Menu", 128, 0, 0)
+Menu::Menu(sf::RenderWindow& window_c, Mouse_controller& mouse_ctrl_c)
+    : window(window_c), mouse_controller(mouse_ctrl_c),
+    menu_title_text("Menu", 128, 0, 0),
+    menu_continue_text("Continue", 86, 0, 0), 
+    menu_option_text("Option", 86, 0, 0),      
+    menu_quit_text("Quit", 86, 0, 0)
 {
     if (!texture.loadFromFile(path)) {
         std::cerr << "Error loading texture from " << path << std::endl;
@@ -12,19 +17,40 @@ Menu::Menu(sf::RenderWindow& window_c) : window(window_c), menu_text("Menu", 128
     center_menu();
 
     sf::FloatRect spriteBounds = sprite.getGlobalBounds();
-    sf::FloatRect textBounds = menu_text.get_text().getGlobalBounds();
 
-    float textXPos = xPos + (spriteBounds.width - textBounds.width) / 2;
-    float textYPos = yPos - 16;
+    // MENU TITLE
+    float titleXPos = xPos + (spriteBounds.width - get_text_bounds(menu_title_text).width) / 2;
+    float titleYPos = yPos - 25; 
+    menu_title_text.set_position(titleXPos, titleYPos);
 
-    menu_text.set_position(textXPos, textYPos);
+    // MENU OPTIONS
+    // CENTER
+    float optionXPos = xPos + (spriteBounds.width - get_text_bounds(menu_option_text).width) / 2;
+    float optionYPos = yPos + (spriteBounds.height / 2) - get_text_bounds(menu_option_text).height / 2;
+    menu_option_text.set_position(optionXPos, optionYPos);
+
+    // MENU CONMTINUE
+    // 75 px above options
+    float continueXPos = xPos + (spriteBounds.width - get_text_bounds(menu_continue_text).width) / 2;
+    float continueYPos = optionYPos - get_text_bounds(menu_continue_text).height - 75; 
+    menu_continue_text.set_position(continueXPos, continueYPos);
+
+    // MENU QUIT
+    // 75 px under options
+    float quitXPos = xPos + (spriteBounds.width - get_text_bounds(menu_quit_text).width) / 2;
+    float quitYPos = optionYPos + get_text_bounds(menu_option_text).height + 75;
+    menu_quit_text.set_position(quitXPos, quitYPos);
 }
 
 // Getter
 
-bool Menu::get_visable()
+bool Menu::get_visable() const
 {
     return visable;
+}
+
+sf::FloatRect Menu::get_text_bounds(Text text) {
+    return text.get_text().getGlobalBounds();
 }
 
 // Setter
@@ -40,20 +66,64 @@ void Menu::draw_menu()
 {
     if (visable)
     {
+        // Update mouse hover detection before drawing
+        update_text_hover();
+
+        // draw menu background
         window.draw(sprite);
-        menu_text.draw_text(window);
+
+        // draw all text
+        menu_title_text.draw_text(window);
+        menu_continue_text.draw_text(window);
+        menu_option_text.draw_text(window);
+        menu_quit_text.draw_text(window);
     }
 }
 
 void Menu::center_menu()
 {
-
     sf::Vector2u windowSize = window.getSize();
-
     sf::Vector2u textureSize = texture.getSize();
 
     xPos = (windowSize.x - (textureSize.x * sprite.getScale().x)) / 2;
     yPos = (windowSize.y - (textureSize.y * sprite.getScale().y)) / 2;
 
     sprite.setPosition(xPos, yPos);
+}
+
+void Menu::update_text_hover()
+{
+    sf::Vector2f mousePosF = mouse_controller.get_mouse_position();
+
+    Text* texts[] = { &menu_continue_text, &menu_option_text, &menu_quit_text };
+
+    for (Text* text : texts)
+    {
+        sf::FloatRect textBounds = text->get_text().getGlobalBounds();
+
+        if (textBounds.contains(mousePosF))
+        {
+            text->get_text().setFillColor(sf::Color(225, 225, 200));
+
+            if (mouse_controller.is_mouse_button_pressed(sf::Mouse::Left))
+            {
+                if (text == &menu_continue_text)
+                {
+                    visable = false;
+                }
+                else if (text == &menu_option_text)
+                {
+                    // Add option menu
+                }
+                else if (text == &menu_quit_text)
+                {
+                    window.close();
+                }
+            }
+        }
+        else
+        {
+            text->get_text().setFillColor(sf::Color::White);
+        }
+    }
 }
