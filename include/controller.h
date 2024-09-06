838278f6e931ea83af05f08e1a583b4f2a3b4207
mork <albers.mark02@gmail.com>
2024-09-06 02:52:29 +0200

    [CHANGE] add window and changed methods

--- include/controller.h
+++ include/controller.h
@@ -5,15 +5,21 @@
 class Mouse_controller
 {
 private:
-	Player& user;
-	sf::Mouse mouse;
+    Player& user;
+    sf::RenderWindow& window;
 
 public:
-	Mouse_controller(Player& player);
-	// Getter
-	sf::Mouse& get_mouse(); 
-	Player& get_player();
+    Mouse_controller(Player& player, sf::RenderWindow& wnd);
 
+    // Getter
+    sf::RenderWindow& get_window();
+    Player& get_player();
+
+    // Methods
+    sf::Vector2f get_mouse_position() const;
+
+    bool is_mouse_button_pressed(sf::Mouse::Button button) const;
+ 
 };
 
 class Keyboard_controller
--- src/controller.cpp
+++ src/controller.cpp
@@ -1,13 +1,24 @@
 #include "controller.h"
 
 // Mouse //
-Mouse_controller::Mouse_controller(Player& player_c) : user(player_c) {}
+Mouse_controller::Mouse_controller(Player& player_c, sf::RenderWindow& window_c) : user(player_c), window(window_c) {}
 
 // Getter
 
-sf::Mouse& Mouse_controller::get_mouse() { return mouse; }
+
+sf::RenderWindow& Mouse_controller::get_window() { return window; }
 
 Player& Mouse_controller::get_player() { return user; }
+
+sf::Vector2f Mouse_controller::get_mouse_position() const
+{
+	return window.mapPixelToCoords(sf::Mouse::getPosition(window));
+}
+
+bool Mouse_controller::is_mouse_button_pressed(sf::Mouse::Button button) const 
+{
+    return sf::Mouse::isButtonPressed(button);
+}
 
 // Keyboard // 
 
