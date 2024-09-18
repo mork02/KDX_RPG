a692df221fc86716ffeea79418de5ae78a8e4378
mork <albers.mark02@gmail.com>
2024-09-18 15:05:14 +0200

    [CHANGE] clean code

--- include/menu.h
+++ include/menu.h
@@ -10,40 +10,43 @@ class Panel;
 class Menu
 {
 private:
-	sf::RenderWindow& window;
-	Input& input;
-	Panel& panel;
+    sf::RenderWindow& window;
+    Input& input;
+    Panel& panel;
 
-	Text menu_title_text;
-	Text menu_continue_text;
-	Text menu_option_text;
-	Text menu_quit_text;
+    Text menu_title_text;
+    Text menu_continue_text;
+    Text menu_option_text;
+    Text menu_quit_text;
 
-	std::string path = "assets/menu_assets/menu/menu.png";
+    std::string path = "assets/menu_assets/menu/menu.png";
 
-	Asset_loader menu_asset;
+    Asset_loader menu_asset;
 
-	float xPos = 0;
-	float yPos = 0;
-	int scale = 6;
-	bool visable;
+    float xPos = 0;
+    float yPos = 0;
+    const int scale = 6;
+    const int vertical_padding = 75;
+    const sf::Color hover_color = sf::Color(210, 170, 109);
+    const sf::Color default_color = sf::Color::White;
+    bool visable;
 
 private:
-	std::vector<std::reference_wrapper<Text>> get_menu_texts();
-	void process_hover(const sf::Vector2f& mousePosF);
-	void process_click(const sf::Vector2f& mousePosF);
+    std::vector<std::reference_wrapper<Text>> get_menu_texts();
+    void process_hover(const sf::Vector2f& mousePosF);
+    void process_click(const sf::Vector2f& mousePosF);
 
-protected:
-	void center_menu(); 
-	void update_text_hover();
+    void center_menu();
+    void position_menu_items();
+    void update_text_hover();
 
 public:
-	Menu(sf::RenderWindow& window, Panel& panel, Input& input);
-	bool get_visable() const;
-	sf::FloatRect get_text_bounds(Text text);
+    Menu(sf::RenderWindow& window, Panel& panel, Input& input);
+    bool get_visable() const;
+    sf::FloatRect get_text_bounds(Text& text); // Pass by reference
 
-	void set_visable(bool value);
-	void draw();
+    void set_visable(bool value);
+    void draw();
 
-	void check_text_click();
+    void check_text_click();
 };
--- src/menu.cpp
+++ src/menu.cpp
@@ -7,39 +7,13 @@ Menu::Menu(sf::RenderWindow& window_c, Panel& panel_c, Input& input_c)
     visable(false),
     menu_asset(window_c, path),
     menu_title_text("Menu", 128, 0, 0),
-    menu_continue_text("Continue", 86, 0, 0), 
-    menu_option_text("Option", 86, 0, 0),      
+    menu_continue_text("Continue", 86, 0, 0),
+    menu_option_text("Option", 86, 0, 0),
     menu_quit_text("Back to Menu", 86, 0, 0)
 {
-
-    menu_asset.get_sprite().setScale((float)scale, (float)scale);
-
+    menu_asset.get_sprite().setScale(static_cast<float>(scale), static_cast<float>(scale));
     center_menu();
-
-    sf::FloatRect spriteBounds = menu_asset.get_sprite().getGlobalBounds();
-
-    // MENU TITLE
-    float titleXPos = xPos + (spriteBounds.width - get_text_bounds(menu_title_text).width) / 2;
-    float titleYPos = yPos - 25; 
-    menu_title_text.set_position((int) titleXPos, (int) titleYPos);
-
-    // MENU OPTIONS
-    // CENTER
-    float optionXPos = xPos + (spriteBounds.width - get_text_bounds(menu_option_text).width) / 2;
-    float optionYPos = yPos + (spriteBounds.height / 2) - get_text_bounds(menu_option_text).height / 2;
-    menu_option_text.set_position((int) optionXPos, (int) optionYPos);
-
-    // MENU CONMTINUE
-    // 75 px above options
-    float continueXPos = xPos + (spriteBounds.width - get_text_bounds(menu_continue_text).width) / 2;
-    float continueYPos = optionYPos - get_text_bounds(menu_continue_text).height - 75; 
-    menu_continue_text.set_position((int) continueXPos, (int) continueYPos);
-
-    // MENU QUIT
-    // 75 px under options
-    float quitXPos = xPos + (spriteBounds.width - get_text_bounds(menu_quit_text).width) / 2;
-    float quitYPos = optionYPos + get_text_bounds(menu_option_text).height + 75;
-    menu_quit_text.set_position((int) quitXPos, (int) quitYPos);
+    position_menu_items(); // Position all text items once
 }
 
 bool Menu::get_visable() const
@@ -47,32 +21,14 @@ bool Menu::get_visable() const
     return visable;
 }
 
-sf::FloatRect Menu::get_text_bounds(Text text) {
-    return text.get_text().getGlobalBounds();
-}
-
 void Menu::set_visable(bool value)
 {
     visable = value;
 }
 
-void Menu::draw()
+sf::FloatRect Menu::get_text_bounds(Text& text)
 {
-    // This method displays everything thats important for menu
-    if (visable)
-    {
-        // Update mouse hover detection before drawing
-        update_text_hover();
-
-        // draw menu background
-        window.draw(menu_asset.get_sprite());
-
-        // draw all text
-        menu_title_text.draw_text(window);
-        menu_continue_text.draw_text(window);
-        menu_option_text.draw_text(window);
-        menu_quit_text.draw_text(window);
-    }
+    return text.get_text().getGlobalBounds();
 }
 
 void Menu::center_menu()
@@ -86,37 +42,60 @@ void Menu::center_menu()
     menu_asset.get_sprite().setPosition(xPos, yPos);
 }
 
+void Menu::position_menu_items()
+{
+    sf::FloatRect spriteBounds = menu_asset.get_sprite().getGlobalBounds();
+
+    // Center title
+    menu_title_text.set_position(
+        static_cast<int>(xPos + (spriteBounds.width - get_text_bounds(menu_title_text).width) / 2),
+        static_cast<int>(yPos - 25)
+    );
+
+    // Center option
+    float optionYPos = yPos + (spriteBounds.height / 2) - get_text_bounds(menu_option_text).height / 2;
+    menu_option_text.set_position(
+        static_cast<int>(xPos + (spriteBounds.width - get_text_bounds(menu_option_text).width) / 2),
+        static_cast<int>(optionYPos)
+    );
+
+    // Position continue 75px above option
+    menu_continue_text.set_position(
+        static_cast<int>(xPos + (spriteBounds.width - get_text_bounds(menu_continue_text).width) / 2),
+        static_cast<int>(optionYPos - get_text_bounds(menu_continue_text).height - 75)
+    );
+
+    // Position quit 75px below option
+    menu_quit_text.set_position(
+        static_cast<int>(xPos + (spriteBounds.width - get_text_bounds(menu_quit_text).width) / 2),
+        static_cast<int>(optionYPos + get_text_bounds(menu_option_text).height + 75)
+    );
+}
 
 std::vector<std::reference_wrapper<Text>> Menu::get_menu_texts()
 {
-    // Returns text to handle click and hover event
     return { std::ref(menu_continue_text), std::ref(menu_option_text), std::ref(menu_quit_text) };
 }
 
 void Menu::process_hover(const sf::Vector2f& mousePosF)
 {
-    auto texts = get_menu_texts(); 
-
-    for (auto& text : texts)
+    for (auto& text : get_menu_texts())
     {
         sf::FloatRect textBounds = text.get().get_text().getGlobalBounds();
-
         if (textBounds.contains(mousePosF))
         {
-            text.get().get_text().setFillColor(sf::Color(210, 170, 109)); // hovered
+            text.get().get_text().setFillColor(sf::Color(210, 170, 109));  // hovered
         }
         else
         {
-            text.get().get_text().setFillColor(sf::Color::White); // defualt
+            text.get().get_text().setFillColor(sf::Color::White);  // default
         }
     }
 }
 
 void Menu::process_click(const sf::Vector2f& mousePosF)
 {
-    auto texts = get_menu_texts();
-
-    for (auto& text : texts)
+    for (auto& text : get_menu_texts())
     {
         sf::FloatRect textBounds = text.get().get_text().getGlobalBounds();
 
@@ -125,16 +104,16 @@ void Menu::process_click(const sf::Vector2f& mousePosF)
             if (&text.get() == &menu_continue_text)
             {
                 set_visable(false);
-                std::cout << "Continue button clicked, visable: " << visable << std::endl;
+                std::cout << "Continue button clicked, visible: " << visable << std::endl;
             }
             else if (&text.get() == &menu_option_text)
             {
-                // Add logic
+                set_visable(false);
             }
             else if (&text.get() == &menu_quit_text)
             {
                 set_visable(false);
-                panel.set_scene(Scene::Main_Menu);  // Switch back to the main menu
+                panel.set_scene(Scene::Main_Menu);
             }
         }
     }
@@ -142,15 +121,33 @@ void Menu::process_click(const sf::Vector2f& mousePosF)
 
 void Menu::update_text_hover()
 {
-    if (visable) {
+    if (visable)
+    {
         sf::Vector2f mousePosF = input.get_mouse_position();
         process_hover(mousePosF);
     }
 }
 
+void Menu::draw()
+{
+    if (visable)
+    {
+        update_text_hover();
+        window.draw(menu_asset.get_sprite());
+
+        for (auto& text : get_menu_texts())
+        {
+            text.get().draw_text(window);
+        }
+    }
+
+}
+
+
 void Menu::check_text_click()
 {
-    if (visable) {
+    if (visable)
+    {
         sf::Vector2f mousePosF = input.get_mouse_position();
         process_click(mousePosF);
     }
