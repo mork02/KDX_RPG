847d02a6f1fe7753c0317356132c51253c546747
mork <albers.mark02@gmail.com>
2024-09-23 02:33:01 +0200

    [CHANGE] renamed class, removed player class

--- include/input.h
+++ include/input.h
@@ -1,25 +1,25 @@
 #pragma once
 #include "SFML/Graphics.hpp"
-#include "player.h"
 
-class Input
+class CInput
 {
 private:
     sf::RenderWindow& window;
     sf::Mouse mouse;
     sf::Keyboard keyboard;
-    Player& user;
 
     bool isKeyPressed = false;
     bool isMousePressed = false;
 
 public:
-    Input(sf::RenderWindow& window, Player& user);
+    CInput(sf::RenderWindow& window);
 
     // Getter
     auto get_mouse() const->sf::Mouse;
     auto get_mouse_position() const->sf::Vector2f;
+
     auto get_keyboard() const->sf::Keyboard;
+
     auto get_isKeyPressed() const -> bool;
     auto get_isMousePressed() const -> bool;
 
@@ -27,6 +27,4 @@ public:
     auto set_isKeyPressed(bool value) -> void;
     auto set_isMousePressed(bool value) -> void;
 
-    // Methods
-    auto is_mouse_button_pressed(sf::Mouse::Button button) const -> bool;
 };
--- src/input.cpp
+++ src/input.cpp
@@ -1,51 +1,44 @@
 #include "input.h"
 
-Input::Input(sf::RenderWindow& window_c, Player& user_c)
-    : window(window_c), user(user_c)
+CInput::CInput(sf::RenderWindow& window_c)
+    : window(window_c)
 {}
 
 // Getter
 
-auto Input::get_mouse() const -> sf::Mouse
+auto CInput::get_mouse() const -> sf::Mouse
 {
     return mouse;
 }
 
-auto Input::get_mouse_position() const -> sf::Vector2f
+auto CInput::get_mouse_position() const -> sf::Vector2f
 {
     return window.mapPixelToCoords(mouse.getPosition(window));
 }
 
-auto Input::get_keyboard() const -> sf::Keyboard
+auto CInput::get_keyboard() const -> sf::Keyboard
 {
     return keyboard;
 }
 
-auto Input::get_isKeyPressed() const -> bool
+auto CInput::get_isKeyPressed() const -> bool
 {
     return isKeyPressed;
 }
 
-auto Input::get_isMousePressed() const -> bool
+auto CInput::get_isMousePressed() const -> bool
 {
     return isMousePressed;
 }
 
 // Setter
 
-auto Input::set_isKeyPressed(bool value) -> void
+auto CInput::set_isKeyPressed(bool value) -> void
 {
     isKeyPressed = value;
 }
 
-auto Input::set_isMousePressed(bool value) -> void
+auto CInput::set_isMousePressed(bool value) -> void
 {
     isMousePressed = value;
-}
-
-// Methods
-
-auto Input::is_mouse_button_pressed(sf::Mouse::Button button) const -> bool
-{
-    return mouse.isButtonPressed(button);
 }
