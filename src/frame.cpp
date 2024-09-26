006b32e6ca1ae43731f7d62f700b8a1626e2507f
mork <albers.mark02@gmail.com>
2024-09-26 04:46:56 +0200

    [CHANGE] rewrite events for menus and scenes

--- src/frame.cpp
+++ src/frame.cpp
@@ -41,36 +41,58 @@ auto CFrame::handle_window_close_event() -> void
     }
 }
 
-auto CFrame::handle_keyboard_input() -> void {
-    // Inputs in Gameplay
-    if (panel.get_scene() == ESceneType::Gameplay) {
+auto CFrame::handle_keyboard_input() -> void
+{
+    if (panel.get_scene() == ESceneType::Gameplay) 
+    {
         // Escape | Pause menu
-        if (event.type == sf::Event::KeyPressed && input.get_keyboard().isKeyPressed(input.get_keyboard().Escape)) {
-            // Check if the current menu is the pause menu, and toggle it
-            if (panel.get_current_menu() == &panel.get_pause_menu()) {
-                panel.set_current_menu(nullptr); // Close the pause menu if it's already open
+        if (event.type == sf::Event::KeyPressed && input.get_keyboard().isKeyPressed(input.get_keyboard().Escape)) 
+        {
+            if (panel.get_current_menu() == &panel.get_pause_menu()) 
+            {
+                panel.set_current_menu(nullptr);
             }
-            else {
-                panel.set_current_menu(&panel.get_pause_menu()); // Open the pause menu if it's not open
+            else 
+            {
+                panel.set_current_menu(&panel.get_pause_menu());
             }
         }
 
         // G | Stats menu
         if (panel.get_current_menu() != &panel.get_pause_menu()) {
-            if (event.type == sf::Event::KeyPressed && input.get_keyboard().isKeyPressed(input.get_keyboard().G)) {
-                // Check if the current menu is the stats menu, and toggle it
-                if (panel.get_current_menu() == &panel.get_stats_menu()) {
-                    panel.set_current_menu(nullptr); // Close the stats menu if it's already open
+            if (event.type == sf::Event::KeyPressed && input.get_keyboard().isKeyPressed(input.get_keyboard().G)) 
+            {
+                if (panel.get_current_menu() == &panel.get_stats_menu()) 
+                {
+                    panel.set_current_menu(nullptr);
                 }
-                else {
-                    panel.set_current_menu(&panel.get_stats_menu()); // Open the stats menu if it's not open
+                else 
+                {
+                    panel.set_current_menu(&panel.get_stats_menu());
                 }
             }
         }
     }
 }
 
+
+
 auto CFrame::handle_mouse_click_event() -> void
 {
-    panel.get_title_screen().handle_click_event(event);
+    if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left)
+    {
+        if (panel.get_scene() == ESceneType::Title_screen) 
+        {
+            panel.get_title_screen().handle_click_event(panel);
+        }
+
+        if (panel.get_scene() == ESceneType::Gameplay)
+        {
+            // Gameplay menus
+            if (panel.get_current_menu()) 
+            {
+                panel.get_current_menu()->handle_click_event(panel);
+            }
+        }
+    }
 }
