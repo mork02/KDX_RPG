281d7d219574a984605d45c6471abe87260c650e
mork <albers.mark02@gmail.com>
2024-10-09 06:15:52 +0200

    [CHANGE] moved update from asset to animation loader and frame dura in constructor

--- include/animation_loader.h
+++ include/animation_loader.h
@@ -3,15 +3,20 @@
 
 class CAnimation_loader
 {
-protected:
+private:
     unsigned const width = 32;
     unsigned const height = 32;
     std::vector<sf::IntRect> frames;
 
-public:
+    unsigned int current_frame = 0;
+    sf::Clock frame_timer;
+    float frame_duration = 0.5f;
+
+protected:
     CAnimation_loader();
 
-    auto setup_animation(const sf::Texture& texture, unsigned frame_width, unsigned frame_height) -> void;
+    auto setup_animation(const sf::Texture& texture, float frame_duration = 0.5f, unsigned frame_width = 32, unsigned frame_height = 32) -> void;
     auto get_frame_rect(unsigned frame_index) -> sf::IntRect;
     auto get_total_frames() -> unsigned;
+    auto update(sf::Sprite& sprite) -> void;
 };
--- src/animation_loader.cpp
+++ src/animation_loader.cpp
@@ -3,7 +3,7 @@
 CAnimation_loader::CAnimation_loader()
 {}
 
-auto CAnimation_loader::setup_animation(const sf::Texture& texture, unsigned frame_width, unsigned frame_height) -> void
+auto CAnimation_loader::setup_animation(const sf::Texture& texture, float frame_duration, unsigned frame_width, unsigned frame_height) -> void
 {
     sf::Vector2u texture_size = texture.getSize();
     for (unsigned y = 0; y < texture_size.y; y += frame_height)
@@ -27,4 +27,14 @@ auto CAnimation_loader::get_frame_rect(unsigned frame_index) -> sf::IntRect
 auto CAnimation_loader::get_total_frames() -> unsigned
 {
     return frames.size();
+}
+
+auto CAnimation_loader::update(sf::Sprite& sprite) -> void
+{
+    if (frame_timer.getElapsedTime().asSeconds() >= frame_duration)
+    {
+        current_frame = (current_frame + 1) % get_total_frames();
+        sprite.setTextureRect(get_frame_rect(current_frame));
+        frame_timer.restart();
+    }
 }
