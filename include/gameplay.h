c1eec7f8a9cedc0c6d7ec76d17fd55653a2c8a51
mork <albers.mark02@gmail.com>
2024-10-16 23:29:11 +0200

    [CHANGE] add player

--- include/gameplay.h
+++ include/gameplay.h
@@ -1,15 +1,17 @@
 #pragma once
 #include "SFML/Graphics.hpp"
 #include "player.h"
-#include "text.h"
+#include <memory>
 #include "asset_loader.h"
 #include "tile_map_loader.h"
+#include <iostream>
 
 class Gameplay
 {
 private:
 	sf::RenderWindow& window;
-	CTile_Map_Loader tile_map;
+
+	std::unique_ptr<CPlayer> player;
 
 	auto loadLevel() -> void;
 
