10f0d62de6590867fcba89ada69a906cd1c96f5b
mork <albers.mark02@gmail.com>
2024-10-16 23:29:05 +0200

    [CHANGE] add playeer

--- src/gameplay.cpp
+++ src/gameplay.cpp
@@ -1,18 +1,18 @@
 #include "gameplay.h"
 
-Gameplay::Gameplay(sf::RenderWindow& window_c) :
-	window(window_c),
-	tile_map("assets/tilemap/level1.png", 7, 3)
+Gameplay::Gameplay(sf::RenderWindow& cWindow) :
+	window(cWindow)
 {
+	player = std::make_unique<CPlayer>();
 	loadLevel();
 }
 
 auto Gameplay::loadLevel() -> void
 {
-	tile_map.addLayer(ELayers::Ground, "assets/tilemap/csv/level1.csv");
+
 }
 
 auto Gameplay::update() -> void
 {
-	tile_map.draw(window);
+
 }
