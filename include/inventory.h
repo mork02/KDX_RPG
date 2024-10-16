cc2aba42623536a42ed9afd17a5ce733202c9a87
mork <albers.mark02@gmail.com>
2024-10-16 23:28:50 +0200

    [CHANGE] removed commands

--- include/inventory.h
+++ include/inventory.h
@@ -9,14 +9,11 @@ private:
     std::vector<Item*> inventory;
 
 public:
-    // Constructor
     Inventory();
 
-    // Getter
     auto get_inventory() -> std::vector<Item*>&;
     auto get_inv_item(int index) -> Item*;
 
-    // Methods
     auto add_item(Item* obj) -> void;
     auto remove_item(Item* obj) -> void;
     auto display_inventory() -> void;
