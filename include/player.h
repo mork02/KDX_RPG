c02e9e9e963ba9a2d34c1c509627973a68395c04
mork <albers.mark02@gmail.com>
2024-10-16 23:25:36 +0200

    [CHANGE] inherited inventory, removed Entity stuff from player

--- include/player.h
+++ include/player.h
@@ -1,35 +1,12 @@
 #pragma once
 #include "entity.h"
 #include "inventory.h"
-#include "item.h"
-#include <memory>
 #include <iostream>
-#include <vector>
 
-class Player : public CEntity
+class CPlayer : public CEntity, public Inventory
 {
 private:
-    std::string name;
-    Inventory inventory;
-
-    Item* weapon_slot = nullptr;
-    Item* armor_slot = nullptr;
 
 public:
-    Player();
-
-    // Getter
-    auto get_name() -> std::string;
-    auto get_inventory() -> Inventory&;
-    auto get_weapon_slot() -> Item*;
-    auto get_armor_slot() -> Item*;
-
-    // Setter
-    auto set_name(std::string new_name) -> void;
-    auto set_weapon_slot(int id) -> void;
-    auto set_armor_slot(int id) -> void;
-
-    // Methods
-    auto display_weapon_slot() -> void;
-    auto display_armor_slot() -> void;
+    CPlayer();
 };
