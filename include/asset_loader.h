ebeefc1ad27baa80806fb1058e3f37fc7d7e3798
mork <albers.mark02@gmail.com>
2024-10-17 20:40:07 +0200

    [CHANGE] add debug things

--- include/asset_loader.h
+++ include/asset_loader.h
@@ -3,6 +3,7 @@
 #include <vector>
 #include "SFML/Graphics.hpp"
 #include "animation_loader.h"
+#include "text.h"
 
 class CAsset_loader : public CAnimation_loader
 {
@@ -11,22 +12,29 @@ private:
     std::string mPath;
     sf::Texture mTexture;
     sf::Sprite mSprite;
+    CText mCoordinates;
 
-    int mX, mY;
+    float mX, mY;
     float mScale = 2.0f;
     bool mIs_Animated;
+    bool mDebug_Mode;
+
+    auto update_Coordinates() -> void;
+    auto debug_Coordinates() -> void;
+    auto debug_Box() -> void;
+    auto debug() -> void;
 
 public:
-    CAsset_loader(sf::RenderWindow& Window, const std::string& Path, bool Is_Animated = false, int X = 0, int Y = 0);
+    CAsset_loader(sf::RenderWindow& Window, const std::string& Path, bool Is_Animated = false, bool Debug_Mode = false);
 
     auto get_texture()  -> sf::Texture&;
     auto get_sprite()   -> sf::Sprite&;
     auto get_Global_sprite_bounds() -> sf::FloatRect;
     auto get_Local_sprite_bounds()  -> sf::FloatRect;
 
-    auto set_scale(float value) -> void;
-    auto center_asset() -> void;
     auto draw() -> void;
 
+    auto center_asset() -> void;
+    auto set_scale(float value) -> void;
     auto set_Position(float x, float y) -> void;
 };
--- src/asset_loader.cpp
+++ src/asset_loader.cpp
@@ -1,10 +1,12 @@
 #include "asset_loader.h"
 
-CAsset_loader::CAsset_loader(sf::RenderWindow& Window, const std::string& Path, bool Is_Animated, int X, int Y)
-    : mWindow(Window), CAnimation_loader(), 
-    mPath(Path), 
-    mIs_Animated(Is_Animated), 
-    mX(X), mY(Y)
+CAsset_loader::CAsset_loader(sf::RenderWindow& Window, const std::string& Path, bool Is_Animated, bool Debug_Mode)
+    : mWindow(Window), CAnimation_loader(),
+    mPath(Path),
+    mIs_Animated(Is_Animated),
+    mX(0), mY(0),
+    mCoordinates("Debug", 16, false, mX, mY),
+    mDebug_Mode(Debug_Mode)
 {
     if (!mTexture.loadFromFile(mPath))
     {
@@ -13,7 +15,7 @@ CAsset_loader::CAsset_loader(sf::RenderWindow& Window, const std::string& Path, 
     }
 
     mSprite.setTexture(mTexture);
-    mSprite.setPosition(static_cast<float>(mX), static_cast<float>(mY));
+    mSprite.setPosition(mX, mY);
     mSprite.setScale(mScale, mScale);
 
     if (mIs_Animated)    setup_animation(mTexture);
@@ -28,10 +30,56 @@ auto CAsset_loader::center_asset() -> void
     mSprite.setPosition(window_size.x / 2.0f, window_size.y / 2.0f);
 }
 
+auto CAsset_loader::debug_Coordinates() -> void
+{
+    std::string text = ("X: " + std::to_string(mX) + " / " + "Y: " + std::to_string(mY));
+    mCoordinates.set_text(text);
+
+    float debug_X = mX;
+    float debug_Y = mY + get_Global_sprite_bounds().height;
+
+    mCoordinates.set_position(debug_X, debug_Y);
+    mCoordinates.draw_text(mWindow);
+}
+
+auto CAsset_loader::debug_Box() -> void
+{
+    if (mDebug_Mode)
+    {
+        sf::FloatRect sprite_bounds = mSprite.getGlobalBounds();
+
+        sf::RectangleShape debug_box(sf::Vector2f(sprite_bounds.width, sprite_bounds.height));
+
+        debug_box.setPosition(sprite_bounds.left, sprite_bounds.top);
+
+        debug_box.setOutlineColor(sf::Color::Blue);
+        debug_box.setOutlineThickness(2.0f);
+
+        debug_box.setFillColor(sf::Color::Transparent);
+
+        mWindow.draw(debug_box);
+    }
+}
+
+auto CAsset_loader::debug() -> void
+{
+    debug_Box();
+    debug_Coordinates();
+}
+
+auto CAsset_loader::update_Coordinates() -> void
+{
+    mX = get_Global_sprite_bounds().getPosition().x;
+    mY = get_Global_sprite_bounds().getPosition().y;
+}
+
 auto CAsset_loader::draw() -> void
 {
     if (mIs_Animated)    update(mSprite);
+    update_Coordinates();
     mWindow.draw(mSprite);
+
+    if (mDebug_Mode) debug();
 }
 
 auto CAsset_loader::get_texture() -> sf::Texture&
