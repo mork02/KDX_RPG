9d6811bcbb52b893792b6fbdc289c6a330724963
mork <albers.mark02@gmail.com>
2024-10-20 23:38:55 +0200

    [CHANGE] entities_movement and mouse_handle method

--- include/gameplay.h
+++ include/gameplay.h
@@ -5,24 +5,26 @@
 #include "tile_map_loader.h"
 #include "player.h"
 #include "entity.h"
+#include "input.h"
 
 class Gameplay
 {
 private:
 	sf::RenderWindow& mWindow;
+	sf::Clock mClock;
+	CInput& mInput;
 
 	std::unique_ptr<CPlayer> mPlayer;
-	std::unique_ptr<CPlayer> mPlayer2;
 
 	const float mMove_Speed = 5.0f;
-
 	std::vector<CEntity*> mShown_Entities = {};
 
 	auto loadLevel() -> void;
 	auto loadEntities() -> void;
 
 public:
-	Gameplay(sf::RenderWindow& Window);
+	Gameplay(sf::RenderWindow& Window, CInput& Input);
 
+	auto handle_mouse_input(sf::Event* current_event) -> void;
 	auto update() -> void;
 };
--- src/gameplay.cpp
+++ src/gameplay.cpp
@@ -1,18 +1,16 @@
 #include "gameplay.h"
 
-Gameplay::Gameplay(sf::RenderWindow& Window) :
-	mWindow(Window)
+Gameplay::Gameplay(sf::RenderWindow& Window, CInput& Input) :
+    mWindow(Window), mInput(Input)
 {
-	mPlayer = std::make_unique<CPlayer>(Window);
-	mPlayer2 = std::make_unique<CPlayer>(Window);
-	loadLevel();
-	loadEntities();
+    mPlayer = std::make_unique<CPlayer>(Window, &Input);
+    loadLevel();
+    loadEntities();
 }
 
 auto Gameplay::loadEntities() -> void
 {
-	mShown_Entities.push_back(mPlayer.get());
-	mShown_Entities.push_back(mPlayer2.get());
+    mShown_Entities.push_back(mPlayer.get());
 }
 
 auto Gameplay::loadLevel() -> void
@@ -20,12 +18,17 @@ auto Gameplay::loadLevel() -> void
 
 }
 
+auto Gameplay::handle_mouse_input(sf::Event* current_event) -> void
+{
+
+}
+
 auto Gameplay::update() -> void
 {
-	for (auto& entities : mShown_Entities)
-	{
-		entities->draw();
-	}
-	mPlayer->set_Position(1, 0);
-	mPlayer2->set_Position(0, 1);
+    float delta_time = mClock.restart().asSeconds();
+    for (auto& entities : mShown_Entities)
+    {
+        entities->draw();
+        entities->handle_movement(delta_time);
+    }
 }
