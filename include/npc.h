330ea439c8e4416bb23f6369882766882e74057b
mork <albers.mark02@gmail.com>
2024-10-23 19:28:59 +0200

    [CHANGE] add row and length

--- include/npc.h
+++ include/npc.h
@@ -18,7 +18,7 @@ private:
 
 public:
     CNPC
-    (sf::RenderWindow& Window, std::string Path, bool IsAnimated, bool Debug_Mode,
+    (sf::RenderWindow& Window, std::string Path, bool IsAnimated, unsigned Row, unsigned Frame_Length,
         std::string Name, int HP, int MaxHP, int BasicDMG, int BasicDEF,
         Zone Zone
     );
