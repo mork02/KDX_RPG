d7fd15b756714ae72a18473f571e2d5827a2db94
mork <albers.mark02@gmail.com>
2024-10-23 19:26:20 +0200

    [CHANGE] enum name. State and lenght

--- include/animation_enum.h
+++ include/animation_enum.h
@@ -1,10 +1,19 @@
 #pragma once
 
-enum class EEntity_animation
+enum class EEntity_State
 {
-	IDLE,
-	UP,
-	RIGHT,
-	DOWN,
-	LEFT
+	IDLE = 1,
+	UP = 3,
+	RIGHT = 3,
+	DOWN = 3,
+	LEFT = 3
+};
+
+enum class EEntity_Length
+{
+	IDLE = 2,
+	UP = 4,
+	RIGHT = 4,
+	DOWN = 4,
+	LEFT = 4
 };
