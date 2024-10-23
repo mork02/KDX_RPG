6dea766ca9bd2d5c16be6b544bcc7efea19613c6
mork <albers.mark02@gmail.com>
2024-10-23 19:29:19 +0200

    [CHANGE] add row and length for asset

--- include/animation_loader.h
+++ include/animation_loader.h
@@ -1,21 +1,20 @@
 #pragma once
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 class CAnimation_loader
 {
 private:
-    unsigned const mWidth = 32;
-    unsigned const mHeight = 32;
     std::vector<sf::IntRect> mFrames;
 
     unsigned int mCurrent_Frame = 0;
+    float mFrame_Duration = 250.f;
     sf::Clock mFrame_Timer;
-    float mFrame_Duration = 0.5f;
 
 protected:
     CAnimation_loader();
 
-    auto setup_animation(const sf::Texture& Texture, float Frame_Duration = 0.5f, unsigned Frame_Width = 32, unsigned Frame_Height = 32) -> void;
+    auto setup_animation(const sf::Texture& Texture, unsigned Row, unsigned Frame_Lenght, unsigned Frame_Width, unsigned Frame_Height) -> void;
     auto get_frame_rect(unsigned frame_index) -> sf::IntRect;
     auto get_total_frames() -> unsigned;
     auto update(sf::Sprite& sprite) -> void;
--- src/animation_loader.cpp
+++ src/animation_loader.cpp
@@ -3,15 +3,21 @@
 CAnimation_loader::CAnimation_loader()
 {}
 
-auto CAnimation_loader::setup_animation(const sf::Texture& Texture, float Frame_Duration, unsigned Frame_Width, unsigned Frame_Height) -> void
+auto CAnimation_loader::setup_animation(const sf::Texture& Texture, unsigned Row, unsigned Frame_Lenght, unsigned Frame_Width, unsigned Frame_Height) -> void
 {
+    mFrames.clear();
+
+    unsigned y = Row * Frame_Height - Frame_Height;
+
     sf::Vector2u texture_size = Texture.getSize();
-    for (unsigned y = 0; y < texture_size.y; y += Frame_Height)
+    int max_frames_in_row = texture_size.x / Frame_Width;
+
+    Frame_Lenght = std::min((int)Frame_Lenght, max_frames_in_row);
+
+    for (unsigned i = 0; i < Frame_Lenght; ++i)
     {
-        for (unsigned x = 0; x < texture_size.x; x += Frame_Width)
-        {
-            mFrames.emplace_back(sf::IntRect(x, y, Frame_Width, Frame_Height));
-        }
+        unsigned x = i * Frame_Width;
+        mFrames.emplace_back(sf::IntRect(x, y, Frame_Width, Frame_Height));
     }
 }
 
@@ -31,7 +37,7 @@ auto CAnimation_loader::get_total_frames() -> unsigned
 
 auto CAnimation_loader::update(sf::Sprite& sprite) -> void
 {
-    if (mFrame_Timer.getElapsedTime().asSeconds() >= mFrame_Duration)
+    if (mFrame_Timer.getElapsedTime().asMilliseconds() >= mFrame_Duration)
     {
         mCurrent_Frame = (mCurrent_Frame + 1) % get_total_frames();
         sprite.setTextureRect(get_frame_rect(mCurrent_Frame));
