e3ca305ef0ac90be08d166635a7dac7905bc6d8f
mork <albers.mark02@gmail.com>
2024-10-30 00:05:03 +0100

    [CHANGE] removed unsused virtual methods

--- include/entity.h
+++ include/entity.h
@@ -13,9 +13,9 @@ private:
     int mBasic_DEF;
 
 public:
-    CEntity(sf::RenderWindow& Window, const std::string& Path, bool Is_Animated, unsigned Row, unsigned Frame_Length, std::string Name, int HP, int Max_HP, int Basic_DMG, int Basic_DEF)
+    CEntity(sf::RenderWindow& Window, std::string Name, int HP, int Max_HP, int Basic_DMG, int Basic_DEF)
         : 
-        CAsset_loader(Window, Path, Is_Animated, Row, Frame_Length),
+        CAsset_loader(Window),
         mName(Name),
         mHP(HP), mMax_HP(Max_HP),
         mBasic_DMG(Basic_DMG), mBasic_DEF(Basic_DEF)
@@ -23,27 +23,27 @@ public:
 
     virtual ~CEntity() = default;
 
-    virtual auto get_name() const -> std::string { return mName; }
+    auto get_name() const -> std::string { return mName; }
     // virtual auto get_id() const -> unsigned { return mID; }
-    virtual auto get_hp() const -> int { return mHP; }
-    virtual auto get_max_hp() const -> int { return mMax_HP; }
-    virtual auto get_basic_dmg() const -> int { return mBasic_DMG; }
-    virtual auto get_basic_defensive() const -> int { return mBasic_DEF; }
+    auto get_hp() const -> int { return mHP; }
+    auto get_max_hp() const -> int { return mMax_HP; }
+    auto get_basic_dmg() const -> int { return mBasic_DMG; }
+    auto get_basic_defensive() const -> int { return mBasic_DEF; }
 
-    virtual auto set_name(std::string Name) -> void { mName = Name; }
-    virtual auto set_hp(int HP) -> void { mHP = HP; }
-    virtual auto set_max_hp(int Max_HP) -> void { mMax_HP = mMax_HP; }
-    virtual auto set_basic_dmg(int Basic_DMG) -> void { mBasic_DMG = Basic_DMG; }
-    virtual auto set_basic_defensive(int Basic_DEF) -> void { mBasic_DEF = Basic_DEF; }
+    auto set_name(std::string Name) -> void { mName = Name; }
+    auto set_hp(int HP) -> void { mHP = HP; }
+    auto set_max_hp(int Max_HP) -> void { mMax_HP = Max_HP; }
+    auto set_basic_dmg(int Basic_DMG) -> void { mBasic_DMG = Basic_DMG; }
+    auto set_basic_defensive(int Basic_DEF) -> void { mBasic_DEF = Basic_DEF; }
 
-    virtual auto take_damage(int Basic_DMG) -> void 
+    auto take_damage(int Basic_DMG) -> void 
     {
         int reduced_damage = Basic_DMG - mBasic_DEF;
         if (reduced_damage < 0) reduced_damage = 0;
         mHP -= reduced_damage;
         if (mHP < 0) mHP = 0;
     }
-    virtual auto heal(int HP) -> void 
+    auto heal(int HP) -> void 
     {
         mHP += HP;
         if (mHP > mMax_HP) mHP = mMax_HP;
