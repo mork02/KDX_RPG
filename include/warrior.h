701e39992271de56a7a8817c41315e0994627427
mork <albers.mark02@gmail.com>
2024-10-30 21:57:27 +0100

    add enum for animations and animations while moving

--- include/warrior.h
+++ include/warrior.h
@@ -2,10 +2,29 @@
 #include "input.h"
 #include "animation_enum.h"
 
+enum EAnimation
+{
+	A_IDLE = 1,
+	L_IDLE = 2,
+
+	A_UP = 3,
+	L_UP = 4,
+
+	A_RIGHT = 3,
+	L_RIGHT = 4,
+
+	A_DOWN = 3,
+	L_DOWN = 4,
+
+	A_LEFT = 3,
+	L_LEFT = 4
+};
+
 #pragma once
 class CWarrior : public CEntity
 {
 private:
+	auto init_Asset() -> void;
 
 public:
 	CWarrior(sf::RenderWindow& Window);
--- src/warrior.cpp
+++ src/warrior.cpp
@@ -1,33 +1,53 @@
 #include "warrior.h"
 
-CWarrior::CWarrior(sf::RenderWindow& Window) : 
+
+CWarrior::CWarrior(sf::RenderWindow& Window) :
     CEntity(Window,
 	"Warrior", 100, 100, 10, 10)
 {
-    mAsset.init("assets/character_assets/AnimationSheet_Character.png", true, 1, 2);
-    mAsset.set_Position(0, 0);
+    init_Asset();
+}
+
+auto CWarrior::init_Asset() -> void
+{
+    mAsset.init("assets/character_assets/AnimationSheet_Character.png", true, EAnimation::A_IDLE, EAnimation::L_IDLE);
+    mAsset.set_debug(true);
     mAsset.set_scale(6);
+    mAsset.set_origin_center();
+    mAsset.get_Sprite().setPosition(500, 500);
 }
 
 auto CWarrior::handle_movement(float delta_time, CInput& Input) -> void
 {
-    float mMovement_Speed = Input.get_Movement_Speed(delta_time);
-    float mZero = 0.f;
+    float Movement_Speed = Input.get_Movement_Speed(delta_time);
+    float Zero = 0.f;
 
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
     {
-        mAsset.set_Position(mZero, -mMovement_Speed); // up
+        mAsset.set_Position(Zero, -Movement_Speed); // up
+        mAsset.set_animation_param(EAnimation::A_UP, EAnimation::L_UP);
+        return;
     }
     if (sf::Keyboard::isKeyPressed (sf::Keyboard::A))
     {
-        mAsset.set_Position(-mMovement_Speed, mZero); // left
+        mAsset.set_Position(-Movement_Speed, Zero); // left
+        mAsset.set_animation_param(EAnimation::A_LEFT, EAnimation::L_LEFT);
+        mAsset.set_direction(false);
+        return;
     }
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
     {
-        mAsset.set_Position(mZero, mMovement_Speed); // down
+        mAsset.set_Position(Zero, Movement_Speed); // down
+        mAsset.set_animation_param(EAnimation::A_DOWN, EAnimation::L_DOWN);
+        return;
     }
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
     {
-        mAsset.set_Position(mMovement_Speed, mZero); // right
+        mAsset.set_Position(Movement_Speed, Zero); // right
+        mAsset.set_animation_param(EAnimation::A_RIGHT, EAnimation::L_RIGHT);
+        mAsset.set_direction(true);
+        return;
     }
+
+    mAsset.set_animation_param(EAnimation::A_IDLE, EAnimation::L_IDLE); // idle
 }
