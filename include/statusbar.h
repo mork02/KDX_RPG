4ebecaacd9514c6dde1d1c4c64bb15354cf05f70
KeDeX <albers.mark02@gmail.com>
2024-11-14 23:55:52 +0100

    add statusbar class

---
+++ include/statusbar.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "asset_loader.h"
+
+class CEntity;
+
+class CStatusBar
+{
+private:
+	CAsset_loader mAsset;
+
+public:
+	CStatusBar(sf::RenderWindow& Window);
+	auto update(CEntity* entity) -> void;
+	auto draw() -> void;
+};
+
---
+++ src/statusbar.cpp
@@ -0,0 +1,21 @@
+#include "statusbar.h"
+#include "entity.h"
+
+CStatusBar::CStatusBar(sf::RenderWindow& Window) :
+	mAsset(Window)
+{
+	mAsset.init_asset()
+}
+
+auto CStatusBar::update(CEntity* entity) -> void
+{
+	if (entity->get_hp() % 25 == 0 && entity->get_hp() <= entity->get_max_hp())
+	{
+		
+	}
+}
+
+auto CStatusBar::draw() -> void
+{
+
+}
