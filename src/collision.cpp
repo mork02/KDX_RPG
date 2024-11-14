298501abafae8c226c284de0d876c2eeec0a44f5
KeDeX <albers.mark02@gmail.com>
2024-11-14 23:56:43 +0100

    add rect overlapping check

--- src/collision.cpp
+++ src/collision.cpp
@@ -2,7 +2,7 @@
 
 auto CCollision::check_collision(CEntity* x, CEntity* y) -> bool
 {
-	if (x->get_Asset().get_Sprite().getGlobalBounds().contains(y->get_Asset().get_Sprite().getGlobalBounds().getPosition()))
+	if (x->get_Asset().get_Sprite().getGlobalBounds().intersects(y->get_Asset().get_Sprite().getGlobalBounds()))
 	{
 		return true;
 	}
