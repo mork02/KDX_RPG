4a22134ba91c6c0f68981a984d3277e46f24aab6
mork <albers.mark02@gmail.com>
2024-11-16 04:03:18 +0100

    add all enums in here

---
+++ include/enums.h
@@ -0,0 +1,40 @@
+#pragma once
+
+enum EGameState
+{
+    TITLE_SCREEN,
+    GAMEPLAY,
+    GAME_OVER
+};
+
+enum class EMenuState
+{
+    NULLPTR,
+    PAUSE,
+    OPTIONS,
+    STATS
+};
+
+enum EAnimation_Warrior
+{
+	A_IDLE = 1,
+	L_IDLE = 2,
+
+	A_UP = 3,
+	L_UP = 4,
+
+	A_RIGHT = 3,
+	L_RIGHT = 4,
+
+	A_DOWN = 3,
+	L_DOWN = 4,
+
+	A_LEFT = 3,
+	L_LEFT = 4
+};
+
+enum class ELayers
+{
+	Ground,
+	Player,
+};
