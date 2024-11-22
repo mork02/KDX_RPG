896b3bf6a0f0de22276872b85f5bf83e326d2317
mork <albers.mark02@gmail.com>
2024-11-22 22:19:57 +0100

    add logic

--- include/state_manager.h
+++ include/state_manager.h
@@ -1,6 +1,26 @@
 #pragma once
+#include <iostream>
+#include "gameplay_state.h"
+#include "title_screen_state.h"
+#include "state.h"
+#include "enums.h"
+
 class CStateManager
 {
-	// TODO: add logic
+private:
+	CState* mState;
+	std::unique_ptr<CGameplay> mGameplay;
+	std::unique_ptr<CTitle_Screen> mTitle_Screen;
+
+public:
+	CStateManager(sf::RenderWindow& Window);
+	auto set_state(CState* State) -> void;
+	auto get_gameplay() -> CGameplay*;
+	auto get_title_screen() -> CTitle_Screen*;
+
+	auto handle_events(sf::Event& Event) -> void;
+	auto update(float delta_time) -> void;
+	auto render() -> void;
+
 };
 
--- src/state_manager.cpp
+++ src/state_manager.cpp
@@ -1,1 +1,46 @@
 #include "state_manager.h"
+
+CStateManager::CStateManager(sf::RenderWindow& Window) :
+	mState(nullptr),
+	mGameplay(std::make_unique<CGameplay>(Window)), 
+	mTitle_Screen(std::make_unique<CTitle_Screen>(Window))
+{}
+
+auto CStateManager::set_state(CState* State) -> void
+{
+	mState = State;
+}
+
+auto CStateManager::get_gameplay() -> CGameplay*
+{
+	return mGameplay.get();
+}
+
+auto CStateManager::get_title_screen()->CTitle_Screen*
+{
+	return mTitle_Screen.get();
+}
+
+auto CStateManager::handle_events(sf::Event& Event) -> void
+{
+	if (mState)
+	{
+		mState->handle_events(Event, *this);
+	}
+}
+
+auto CStateManager::update(float delta_time) -> void
+{
+	if (mState)
+	{
+		mState->update(delta_time);
+	}
+}
+
+auto CStateManager::render() -> void
+{
+	if (mState)
+	{
+		mState->render();
+	}
+}
