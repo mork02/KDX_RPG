bbe0fc1265efec310f89c01fc47c3cf9875e6f6c
mork <albers.mark02@gmail.com>
2024-12-02 18:46:53 +0100

    add txm files

---
+++ assets/map/tilemap.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="tilemap" tilewidth="32" tileheight="32" tilecount="160" columns="10">
+ <image source="tilemap.png" width="320" height="512"/>
+</tileset>
